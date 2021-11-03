7259eaedd8079bd8b26c9d1a00cbc904daa41fa5
Joan Maina <mainajoan12@gmail.com>
2021-11-03 14:13:26 +0300

    add login route

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express"); 
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
+const bcrypt = require('bcryptjs')
 
 require('dotenv').config();
 
 const app = express();
 
 const { signUpValidation, encryptPassword } = require("./helpers")
-
 
 app.use(express.json())
 
@@ -29,13 +29,12 @@ app.post('/auth/register', async (req, res) => {
     try {
       users.push({id, email, username, pass});
 
-      jwt.sign({ username, email, id }, process.env.SECRETKEY, (err, token) => {
-        if (err) return res.status(500).json({ message: "Internal Server Error" });
-        return res.status(200).json({ token, message: "User registered successfully" });
-      });
+      return res.status(201).send( {message: "User registered successfully" });
 
     } catch (err) {
+
       res.status(500).send({ message: "Internal Server Error" });
+
     }
 });
 
@@ -43,8 +42,45 @@ app.get('/users', (req, res) => {
     res.send({users});
 })
 
+app.post('/auth/login', async(req, res) => {
+  try{
+
+    const { email, password } = req.body;
+    const user = users.find(user => user.email === email);
+    
+    if(!user){
+
+      return res.status(401).json({message: 'no such user exists'});
+
+    }else{
+      console.log(user.pass)
+      let auth = await bcrypt.compare(password, user.pass)
+       
+console.log(auth);
+  if(auth == false)res.status(401).send('wrong inputs')
+
+ // if (err) return res.status(500).json({ message: "Internal Server Error" });
+
+  // jwt.sign({ username, email, id }, process.env.SECRETKEY, (err, token) => {
+  //   if (err) return res.status(500).json({ message: "Internal Server Error" });
+  //   return res.status(200).json({ token, message: "User registered successfully" });
+  // });
+  jwt.sign({ email: user.email, username: user.username, password: user.password }, process.env.SECRETKEY, (err, token) => {
+    return res.status(200).json({
+      message: "login sucessful",
+      token
+    });
+  });
+
+
+}
+}catch(error){
+    res.status(500).send('jo');
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App running on port:${PORT}`)
-})
+});
